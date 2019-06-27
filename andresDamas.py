31493d18b1c01b9df6443dc82f3b1ba717a43fab
Ronny Cajas <49683394+Frightbane@users.noreply.github.com>
2019-06-27 17:02:28 -0500

    Add files via upload

---
+++ andresDamas.py
@@ -0,0 +1,47 @@
+from Tkinter import *
+ventana = Tk()
+frame = Frame()
+frame.pack()
+
+def posin(event):
+    info = event.widget.grid_info()
+    print((info["row"], info["column"]))
+
+def casillas(texto, colortexto, color, fila, columna):
+    casilla = Button(frame, text = texto)
+    casilla.grid(row = fila, column = columna)
+    casilla.config(bg = color, fg = colortexto, width = 5)
+
+    #
+    casilla.bind("<Button 1>",posin)
+
+def tablero():
+    for f in range(2, 10):
+        for c in range(2, 10):
+
+            if((f%2 != 0) and (c%2 != 0)): 
+                casillas(" ", 'black', 'black', f, c)
+            elif((f%2 == 0) and (c%2 == 0)):
+                casillas(" ", 'black', 'black', f, c)
+            else:
+                casillas(" ", 'white', 'white', f, c)
+            
+
+def clickNuevoJuego():
+    for f in range(2, 10):
+        for c in range(2, 10):
+            if(f<5):
+                if((f%2 != 0) and (c%2 != 0)):
+                    casillas("x", 'red', 'black', f, c)
+                elif((f%2 == 0) and (c%2 == 0)):
+                    casillas("x", 'red','black', f, c)
+            if(f>6):
+                if((f%2 != 0) and (c%2 != 0)):
+                    casillas("x", 'white', 'black', f, c)
+                if((f%2 == 0) and (c%2 == 0)):
+                    casillas("x", 'white', 'black', f, c)
+
+tablero()
+
+clickNuevoJuego()
+ventana.mainloop()
